6f4d3d672e333cac53d5bdff7c222c25795e7982
Soo-Beom <soobeomkim77@gmail.com>
2021-02-22 11:17:18 -0800

    commit tree 48056db284e5b10e6e4f5d3ff249fccb133940eb

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper'; 
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` ;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+const initialCurrentUser = currentUser.toUpperCase(0);
+const shortGreeting = `Welcome, ${initialCurrentUser}!`;
+
+
+
